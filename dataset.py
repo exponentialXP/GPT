c4908af08765658ec3445f6b73da93c41de35833
exponentialXP <127988888+exponentialXP@users.noreply.github.com>
2024-01-01 13:10:59 +0000

    Changed chars to examples

--- dataset.py
+++ dataset.py
@@ -4,30 +4,30 @@ import torch
 from datasets import load_dataset
 
 dataset = load_dataset('Skylion007/openwebtext', streaming=True)
-max_chars = 100_000_000
-max_val_chars = 10_000_000
+max_examples = 10_000
+max_val_examples = 500
+print_frequency = 100
 output_location = 'raw-input.txt'
 val_output_location = 'raw-val-input.txt'
 
-num_chars = 0
+num_examples = 0
 with open(output_location, 'w', encoding='utf-8') as train_file, open(val_output_location, 'w', encoding='utf-8') as val_file:
     for example_chunk in dataset['train']:
-        for example in example_chunk['text']:
+        example = example_chunk['text']
+        if num_examples % print_frequency == 0:
+            print(f"{num_examples:,} examples written")
 
-            if num_chars % 1_000_000 == 0:
-                print(f"{num_chars/1_000_000}MB written")
+        if num_examples < max_examples:
+            train_file.write(f"{example}")
+            num_examples += 1
 
-            if num_chars < max_chars:
-                    train_file.write(example)
-                    num_chars += 1
-
-            elif num_chars < max_chars + max_val_chars:
-                    val_file.write(example)
-                    num_chars += 1
-            else:
-                break
+        elif num_examples < max_examples + max_val_examples:
+            val_file.write(f"{example}")
+            num_examples += 1
+        else:
+            break
         
-        if num_chars >= max_chars + max_val_chars:
+        if num_examples >= max_examples + max_val_examples:
             break
             
         
@@ -42,6 +42,7 @@ tokenizer = ByteLevelBPETokenizer()
 tokenizer.train([output_location, val_output_location], vocab_size=12000)
 tokenizer.save('tokenizer.json')
 
+print("Tokenizing...")
 tokenized_train_text = tokenizer.encode(train_text).ids
 tokenized_val_text = tokenizer.encode(val_text).ids
 torch.save(tokenized_train_text, 'tokenized_train_text.pt')
