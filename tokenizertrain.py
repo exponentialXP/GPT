876c892ef9c721441b51ef852791118b5fda0566
exponentialXP <127988888+exponentialXP@users.noreply.github.com>
2024-01-02 19:07:40 +0000

    Progress Bar + option to load temp.txt file

--- tokenizertrain.py
+++ tokenizertrain.py
@@ -1,25 +1,28 @@
 from datasets import load_dataset 
 dataset = load_dataset('openwebtext', streaming=True)
 from tokenizers import ByteLevelBPETokenizer
+from tqdm import tqdm
 
 tokenizer = ByteLevelBPETokenizer()
 text = []
-
 examples_to_train = 1_000_000
+load_txt_file = False
 
-for i, example in enumerate(dataset['train']):
-    example_text = example['text']
-    text.append(example_text)
-    del example_text
+if load_txt_file == False:
+    pbar = tqdm(total=examples_to_train, desc="Processing text")
+    for i, example in enumerate(dataset['train']):
+        example_text = example['text']
+        text.append(example_text)
+        del example_text
 
-    if i > examples_to_train:
-        break
+        if i > examples_to_train:
+            break
 
-    if i % 1_000 == 0:
-        print(f"Processed {i} examples")
+        pbar.update(1)
 
-with open('temp.txt', 'w', encoding='utf-8') as f:
-    f.writelines(text)
+    print("Writing text to file...")
+    with open('temp.txt', 'w', encoding='utf-8') as f:
+        f.writelines(text)
 
 tokenizer.train(['temp.txt'], vocab_size=32_000, min_frequency=2, special_tokens=['<|endoftext|>', '<|$USER|>', '<|$ASSISTANT|>'])
 tokenizer.save('tokenizer.json')
