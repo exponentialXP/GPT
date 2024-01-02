00599ddf9dadbe05bf4a4734620e239cd6281dbb
exponentialXP <127988888+exponentialXP@users.noreply.github.com>
2024-01-02 17:16:25 +0000

    New features + Brushups

--- sample.py
+++ sample.py
@@ -4,6 +4,8 @@ import os
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+# torch.manual_seed(42)
+
 if os.path.exists('modelsave.pt'):
     checkpoint = torch.load('modelsave.pt')
     args = checkpoint['args']
@@ -21,14 +23,16 @@ else:
 
 model.eval()
 context = "<|endoftext|>"
-max_new_tokens = args.window_size 
-num_samples = 5
+max_new_tokens = 256
+top_k = 200 # See if setting this back to 200 helps 
+num_samples = 1
+temperature = 0.8 
 
 x = torch.tensor(tokenizer.encode(context).ids, dtype=torch.long, device=device).unsqueeze(0)
 
 with torch.no_grad():
     with torch.amp.autocast(device_type=device, dtype=torch.bfloat16):
         for k in range(num_samples):
-            y = model.generate(x, max_new_tokens)
+            y = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k)
             print(tokenizer.decode(y[0].tolist()))
             print('---------------')
