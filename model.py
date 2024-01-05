3322588d94486e4884c90c193ad18be92d2c4375
exponentialXP <127988888+exponentialXP@users.noreply.github.com>
2024-01-05 17:01:20 +0000

    Top-P sampling and options to display top token probabilities!

--- model.py
+++ model.py
@@ -14,17 +14,14 @@ class ModelArgs:
     vocab_size: int = -1
     device: str = 'cpu'
 
-class RMSNorm(nn.Module):
-    def __init__(self, dim: int, eps: float = 1e-6):
+class LayerNorm(nn.Module):
+    def __init__(self, ndim):
         super().__init__()
-        self.eps = eps
-        self.weight = nn.Parameter(torch.ones(dim))
+        self.weight = nn.Parameter(torch.ones(ndim))
+        self.bias = None
 
-    def _norm(self, x: torch.Tensor):
-        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
-
-    def forward(self, x: torch.Tensor):
-        return self.weight * self._norm(x.float()).type_as(x)
+    def forward(self, input):
+        return F.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-5)
     
 class Attention(nn.Module):
     def __init__(self, args: ModelArgs):
@@ -68,9 +65,9 @@ class FeedForward(nn.Module):
 class Block(nn.Module):
     def __init__(self, args: ModelArgs):
         super().__init__()
-        self.norm1 = RMSNorm(args.emb_dim)
+        self.norm1 = LayerNorm(args.emb_dim)
         self.attn = Attention(args)
-        self.norm2 = RMSNorm(args.emb_dim)
+        self.norm2 = LayerNorm(args.emb_dim)
         self.mlp = FeedForward(args)
     
     def forward(self, x):
@@ -86,6 +83,7 @@ class Model(nn.Module):
         self.pos_emb = nn.Embedding(args.window_size, args.emb_dim)
         self.dropout = nn.Dropout(args.dropout)
         self.blocks = nn.ModuleList([Block(args) for _ in range(args.n_layers)])
+        self.ln_f = nn.LayerNorm(args.emb_dim)
         self.lm_head = nn.Linear(args.emb_dim, args.vocab_size)
 
         self.tok_emb.weight = self.lm_head.weight
@@ -109,6 +107,7 @@ class Model(nn.Module):
         x = self.dropout(tok + pos)
         for block in self.blocks:
             x = block(x)
+        x = self.ln_f(x)
         logits = self.lm_head(x)
 
         if targets is None:
@@ -125,15 +124,24 @@ class Model(nn.Module):
         return sum(p.numel() for p in self.parameters()) - self.pos_emb.weight.numel()
     
     @torch.no_grad()
-    def generate(self, x, max_new_tokens=500, temperature=1.0, top_k=200):
+    def generate(self, x, max_new_tokens=500, temperature=1.0, p=None):
         for _ in range(max_new_tokens):
             x_trim = x[:, -self.args.window_size:]
             logits, _ = self(x_trim)
             logits = logits[:, -1, :] / temperature
-            if top_k is not None:
-                v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
-                logits[logits < v[:, [-1]]] = -float('Inf') 
-            probs = F.softmax(logits, dim=-1) 
+            if p is not None:
+                probs, indices = torch.sort(F.softmax(logits, dim=-1), descending=True)
+                probs_cumulative = torch.cumsum(probs, dim=0)
+                mask = probs_cumulative - probs > p
+                probs[mask] = 0.0
+                probs.div_(probs.sum(dim=-1, keepdim=True))
+            # max_displayed_probs = 60
+            # from tokenizers import Tokenizer
+            # tokenizer = Tokenizer.from_file('./tokenizer.json')
+            # for i, (prob, index) in enumerate(zip(probs[0][:], indices[0][:])):
+            #     print(f"Token: {tokenizer.id_to_token(index)}, Prob: {prob}")
+            #     if i > max_displayed_probs or prob == 0:
+            #         break
             x_next = torch.multinomial(probs, num_samples=1) 
             x = torch.cat((x, x_next), dim=1) 
         return x
