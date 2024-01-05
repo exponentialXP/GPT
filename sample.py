b00dc8f363e37a9374ba73c458c37d12f8f31684
exponentialXP <127988888+exponentialXP@users.noreply.github.com>
2024-01-05 19:41:38 +0000

    Asd]

--- sample.py
+++ sample.py
@@ -6,24 +6,30 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 dtype = torch.bfloat16 if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else torch.float16
 # torch.manual_seed(42)
 
-if os.path.exists('modelsave.pt'):
-    checkpoint = torch.load('modelsave.pt')
+save_path = 'modelsave.pt'
+tokenizer_path = 'tokenizer.json'
+
+if os.path.exists(save_path):
+    checkpoint = torch.load(save_path)
     args = checkpoint['args']
     model = Model(args)
     model.to(device)
     model.load_state_dict(checkpoint['model_params'])
-else:
-    exit("!!<<modelsave.pt (Model Checkpoint) not found!>>!!")
+    print(f"Resuming from iter {checkpoint['iter']:,}\nParameters: {sum(p.numel() for p in model.parameters()) - model.pos_emb.weight.numel():,}")
 
-print(f"Resuming from iter {checkpoint['iter']:,}\nParameters: {sum(p.numel() for p in model.parameters()) - model.pos_emb.weight.numel():,}")
-if os.path.exists('tokenizer.json'):
+else:
+    exit("!!<<Model Checkpoint not found!>>!!")
+
+if os.path.exists(tokenizer_path):
     from tokenizers import Tokenizer
-    tokenizer = Tokenizer.from_file('tokenizer.json')
+    tokenizer = Tokenizer.from_file(tokenizer_path)
 else:
     exit("!!<<Tokenizer.json not found!>>!")
 
-context = """<|endoftext|>"""
-max_new_tokens = 1
+context = """: In a shocking finding, scientist discovered a herd of unicorns living
+in a remote, previously unexplored valley, in the Andes Mountains. Even more surprising to the
+researchers was the fact that the unicorns spoke perfect English"""
+max_new_tokens = 256
 p = .9
 num_samples = 1
 temperature = .8
