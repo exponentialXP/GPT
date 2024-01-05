6ddb230f4d18be244bc48f116503896466e75526
exponentialXP <127988888+exponentialXP@users.noreply.github.com>
2024-01-05 17:03:02 +0000

    Update sample.py

--- sample.py
+++ sample.py
@@ -3,7 +3,7 @@ import torch
 import os
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
+dtype = torch.bfloat16 if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else torch.float16
 # torch.manual_seed(42)
 
 if os.path.exists('modelsave.pt'):
@@ -15,24 +15,24 @@ if os.path.exists('modelsave.pt'):
 else:
     exit("!!<<modelsave.pt (Model Checkpoint) not found!>>!!")
 
+print(f"Resuming from iter {checkpoint['iter']:,}\nParameters: {sum(p.numel() for p in model.parameters()) - model.pos_emb.weight.numel():,}")
 if os.path.exists('tokenizer.json'):
     from tokenizers import Tokenizer
     tokenizer = Tokenizer.from_file('tokenizer.json')
 else:
     exit("!!<<Tokenizer.json not found!>>!")
 
-model.eval()
-context = "<|endoftext|>"
+context = """<|endoftext|>""" # What are the next token(s) the model should predict based off the context?
 max_new_tokens = 256
-top_k = 200 # See if setting this back to 200 helps 
+p = .9
 num_samples = 1
-temperature = 0.8 
+temperature = 1
 
 x = torch.tensor(tokenizer.encode(context).ids, dtype=torch.long, device=device).unsqueeze(0)
 
 with torch.no_grad():
-    with torch.amp.autocast(device_type=device, dtype=torch.bfloat16):
+    with torch.amp.autocast(device_type=device, dtype=dtype):
         for k in range(num_samples):
-            y = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k)
+            y = model.generate(x, max_new_tokens, temperature=temperature, p=p)
             print(tokenizer.decode(y[0].tolist()))
             print('---------------')
