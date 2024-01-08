12aa34a5dcab15456d03a8aa4f17c14bb2e164dd
exponentialXP <127988888+exponentialXP@users.noreply.github.com>
2024-01-08 16:10:05 +0000

    Changed tokenizer from Huggingface BPE -> Sentencepiece BPE

--- sample.py
+++ sample.py
@@ -1,16 +1,20 @@
 from model import Model
 import torch
 import os
+from tqdm import tqdm
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 dtype = torch.bfloat16 if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else torch.float16
-seed = 42 # None to make it random every time!
+
+seed = None # None to make it random every time!
+mode = 'print' # write to write to file, print to print text
+write_file_path = 'generation.txt'
 
 if seed is not None:
     torch.manual_seed(42)
 
 save_path = 'modelsave.pt'
-tokenizer_path = 'tokenizer.json'
+tokenizer_path = 'tokenizer.model'
 
 if os.path.exists(save_path):
     checkpoint = torch.load(save_path)
@@ -23,25 +27,35 @@ if os.path.exists(save_path):
 else:
     exit("!!<<Model Checkpoint not found!>>!!")
 
+import os
 if os.path.exists(tokenizer_path):
-    from tokenizers import Tokenizer
-    tokenizer = Tokenizer.from_file(tokenizer_path)
+    from sentencepiece import SentencePieceProcessor
+    tokenizer = SentencePieceProcessor(model_file=tokenizer_path)
+    vocab_size = tokenizer.vocab_size()
 else:
-    exit("!!<<Tokenizer.json not found!>>!")
+    exit("!!<<No tokenizer found>>!!")
 
-context = """: In a shocking finding, scientist discovered a herd of unicorns living
-in a remote, previously unexplored valley, in the Andes Mountains. Even more surprising to the
-researchers was the fact that the unicorns spoke perfect English"""
-max_new_tokens = 256
+context = """<|endoftext|>"""
+max_new_tokens = 3
 p = .9
 num_samples = 1
 temperature = .8
 
-x = torch.tensor(tokenizer.encode(context).ids, dtype=torch.long, device=device).unsqueeze(0)
+x = torch.tensor(tokenizer.Encode(context), dtype=torch.long, device=device).unsqueeze(0)
 
-with torch.no_grad():
-    with torch.amp.autocast(device_type=device, dtype=dtype):
-        for k in range(num_samples):
-            y = model.generate(x, max_new_tokens, temperature=temperature, p=p, view_probabilites=False)
-            print(tokenizer.decode(y[0].tolist()))
-            print('---------------')
+if mode == 'print':
+    with torch.no_grad():
+        with torch.amp.autocast(device_type=device, dtype=dtype):
+            for k in tqdm(range(num_samples), desc="Generating samples..."):
+                y = model.generate(x, max_new_tokens, temperature=temperature, p=p, view_probabilites=False)
+                print(tokenizer.decode(y[0].tolist()))
+                print('\n---------------\n')
+
+elif mode == 'write':
+    with open(write_file_path, 'w', encoding='utf-8') as f:
+        with torch.no_grad():
+            with torch.amp.autocast(device_type=device, dtype=dtype):
+                for k in tqdm(range(num_samples), desc="Generating samples..."):
+                    y = model.generate(x, max_new_tokens, temperature=temperature, p=p, view_probabilites=False)
+                    f.write(tokenizer.decode(y[0].tolist()))
+                    f.write('\n---------------\n')
