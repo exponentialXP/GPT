b73b466e013ff8d4a36b8907871362fa78d51670
exponentialXP <127988888+exponentialXP@users.noreply.github.com>
2024-01-14 18:14:40 +0000

    New Tokenizer, fixed possible errors

--- dataset.py
+++ dataset.py
@@ -1,20 +1,23 @@
 from datasets import load_dataset
 from tqdm import tqdm
 
-max_examples = 1_000_000 # 1 example = 1,000 tokens
-dset = load_dataset("openwebtext", split=f'train[0:{max_examples}]', cache_dir='./cache')
+max_examples = 800_000 # 1 example = 1,000 tokens 
+test_size = 0.01
+dataset = load_dataset("openwebtext", split=f'train[0:{max_examples}]', cache_dir='./cache')
 
-split_dataset = dset.train_test_split(test_size=0.05, seed=42, shuffle=True)
+split_dataset = dataset.train_test_split(test_size=test_size, seed=42, shuffle=True)
 split_dataset['val'] = split_dataset.pop('test')
 
-from sentencepiece import SentencePieceProcessor
-tokenizer_path = 'tokenizer.model'
-tokenizer = SentencePieceProcessor(model_file=tokenizer_path)
+from tokenizers import Tokenizer
+tokenizer = Tokenizer.from_file('tokenizer.json')
 
-eot_id = tokenizer.Encode('<|endoftext|>')
+def Encode(str):
+    return (tokenizer.encode(str)).ids
+
+eot_id = Encode('<|endoftext|>')
 
 def process(example):
-    tokens = tokenizer.Encode(example['text']) + eot_id
+    tokens = Encode(example['text']) + eot_id
     return {'tokens': tokens, 'len': len(tokens)}
 
 tokenized = split_dataset.map(
@@ -30,7 +33,10 @@ for split, example in tokenized.items():
     filename = f"{split}.bin"
     map = np.memmap(filename, dtype=np.uint16, mode='w+', shape=(length,))
 
-    total_batches = 128
+    total_batches = min(max_examples*test_size, 512)
+    if total_batches < 1:
+        exit("!!<<Number of batches too small>>!!")
+
     start_idx = 0
 
     for ix in tqdm(range(total_batches), desc=f'Writing {filename}'):
